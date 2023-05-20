c5ec178fc70d2e8eb275eebd6ad1f2b4263d6d9d
marjanhasan <moonmarjanhasan@gmail.com>
2023-05-20 13:44:03 +0600

    post and get api added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gexkyvp.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vucpekr.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,6 +25,35 @@ async function run() {
     await client.connect();
 
     //StartYOurCOde
+    const db = client.db("kidzed");
+    const toysCollection = db.collection("toys");
+
+    app.post("/addToys", async (req, res) => {
+      const body = req.body;
+      const result = await toysCollection.insertOne(body);
+      res.send(result);
+    });
+
+    app.get("/allToys/:text", async (req, res) => {
+      //   console.log(req.params.text);
+      if (
+        req.params.text == "math" ||
+        req.params.text == "language" ||
+        req.params.text == "science"
+      ) {
+        const result = await toysCollection
+          .find({ subCategory: req.params.text })
+          .toArray();
+        return res.send(result);
+      }
+      const result = await toysCollection.find({}).toArray();
+      res.send(result);
+    });
+
+    // app.get("/allToys", async (req, res) => {
+    //   const result = await toysCollection.find({}).toArray();
+    //   res.send(result);
+    // });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
